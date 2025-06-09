2aca023677ac91ca24d1a5c571d299b19c81297f
Y M Horn <ymh53r@gmail.com>
2025-06-09 17:50:07 +0300

    Basic outline of recieving reports

---
+++ Reporters/InputReport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reporters
+{
+    internal static class InputReport
+    {
+        public static Report input()
+        {
+            Report report = new Report();
+            Console.WriteLine("");
+            report.ReporterName = Console.ReadLine();
+            Console.WriteLine("");
+            report.ReportedName = Console.ReadLine();
+            Console.WriteLine("");
+            report.ReportText = Console.ReadLine();
+            report.DateTime = DateTime.Now;
+
+            return report;
+        }
+    }
+}
---
+++ Reporters/Report.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reporters
+{
+    internal class Report
+    {
+        public string ReporterName { get; set; }
+        public string ReportedName { get; set; }
+        public string ReportText {  get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
